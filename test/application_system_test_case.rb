6517c5ef7e27efb43fb8bdaf05f9c7d69cc47099
seinoazumi <seinoazumi@gmail.com>
2019-09-04 02:21:46 +0000

    commit the files remaind in cap:4

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+# class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+#   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  
+# end
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         "Ruby on Rails Tutorial sample app"
+    assert_equal full_title("Help"), "Test|Ruby on Rails Tutorial sample app"
+  end
 end
